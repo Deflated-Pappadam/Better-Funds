8f09549599dd180e5855df4116fc7c1ee76cedac
Ritin George <ritingeorge.d@gmail.com>
2024-02-25 00:30:30 +0530

    typo: fix text errors [business-dash]

--- app/business/dashboard/page.tsx
+++ app/business/dashboard/page.tsx
@@ -21,30 +21,6 @@ import {
 import { db } from "@/firebase";
 import { Button } from "@/components/ui/button";
 
-const Businesses = [
-  {
-    name: "Business 1",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac tortor tellus. Donec vitae ultricies nibh. Duis fermentum orci lectus, ut fermentum libero malesuada non. Quisque accumsan blandit molestie. Cras faucibus, mi nec suscipit posuere, mi mi bibendum libero, ut pellentesque lacus tortor nec quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque. Maecenas blandit hendrerit metus, a sollicitudin urna venenatis ut.",
-    investedAmount: "$250.00",
-    goalAmount: "$1000.00",
-  },
-  {
-    name: "Business 2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac tortor tellus. Donec vitae ultricies nibh. Duis fermentum orci lectus, ut fermentum libero malesuada non. Quisque accumsan blandit molestie. Cras faucibus, mi nec suscipit posuere, mi mi bibendum libero, ut pellentesque lacus tortor nec quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque. Maecenas blandit hendrerit metus, a sollicitudin urna venenatis ut.",
-    investedAmount: "$250.00",
-    goalAmount: "$1000.00",
-  },
-  {
-    name: "Business 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac tortor tellus. Donec vitae ultricies nibh. Duis fermentum orci lectus, ut fermentum libero malesuada non. Quisque accumsan blandit molestie. Cras faucibus, mi nec suscipit posuere, mi mi bibendum libero, ut pellentesque lacus tortor nec quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque. Maecenas blandit hendrerit metus, a sollicitudin urna venenatis ut.",
-    investedAmount: "$250.00",
-    goalAmount: "$1000.00",
-  },
-];
-
 type docData = {
   id: string;
   value: DocumentData;
@@ -100,15 +76,23 @@ function Page() {
           <div className="flex flex-col w-full gap-10 py-5">
             <div className="flex md:flex-row flex-col w-full justify-between">
               <div>
-                <h1 className="text-[4vw]">Rhon S George</h1>
+                <h1 className="text-[4vw]">Shibu RP</h1>
                 <h2 className="text-[2vw]">Entrepreneur</h2>
-                <Button className="mt-5" >Ideate</Button>
+                <Button className="mt-5">Ideate</Button>
               </div>
               <div className="flex flex-col md:w-[30%] h-[275px]  justify-center border-[#38383848] border-2 rounded-xl  poppins-medium text-xl p-5 m-2">
                 My Numbers
                 <div className="grid grid-cols-2 p-3">
                   <div className="p-4">
-                    <h1 className="text-2xl">{data ? Array.from(data.values()).reduce((acc, data) => acc + data.value.totalContributed, 0) : 'loading'} $</h1>
+                    <h1 className="text-2xl">
+                      {data
+                        ? Array.from(data.values()).reduce(
+                            (acc, data) => acc + data.value.totalContributed,
+                            0
+                          )
+                        : "loading"}{" "}
+                      $
+                    </h1>
                     <h2 className="text-sm text-[#3d3d3dba]">Total</h2>
                   </div>
                   <div className="p-4">
@@ -116,7 +100,12 @@ function Page() {
                     <h2 className="text-sm text-[#3d3d3dba]">Growth</h2>
                   </div>
                   <div className="p-4">
-                    <h1 title={walletAddress ? walletAddress : 'loading'} className="text-2xl text-ellipsis truncate">{walletAddress ? walletAddress : 'loading'}</h1>
+                    <h1
+                      title={walletAddress ? walletAddress : "loading"}
+                      className="text-2xl text-ellipsis truncate"
+                    >
+                      {walletAddress ? walletAddress : "loading"}
+                    </h1>
                     <h2 className="text-sm text-[#3d3d3dba]">Wallet</h2>
                   </div>
                 </div>
@@ -124,13 +113,13 @@ function Page() {
             </div>
             <div className="flex justify-between">
               <div className="w-full h-[400px] overflow-y-scroll border-[#38383848] border-2 rounded-xl p-5 ">
-                <h1 className="text-[1.5vw]">My Products</h1>
+                <h1 className="text-[1.5vw]">My Ventures</h1>
                 <Table className="my-2">
                   <TableHeader>
                     <TableRow>
-                      <TableHead className="w-[120px]">Business</TableHead>
+                      <TableHead className="w-[120px]">Project</TableHead>
                       <TableHead>Description</TableHead>
-                      <TableHead>Invested</TableHead>
+                      <TableHead>Achieved</TableHead>
                       <TableHead className="text-right">Goal</TableHead>
                     </TableRow>
                   </TableHeader>
