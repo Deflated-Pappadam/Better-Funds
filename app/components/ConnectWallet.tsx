68c7c3ea43a82f178d56053f6724963abde2b6c3
Ritin George <ritingeorge.d@gmail.com>
2024-02-25 00:31:30 +0530

    typo: Connect Wallet text

--- app/components/ConnectWallet.tsx
+++ app/components/ConnectWallet.tsx
@@ -9,7 +9,7 @@ function ConnectWallet({ click }: ConnectWalletProps) {
     <div className='min-h-screen flex flex-col w-full justify-center items-center text-[#2d2d2d] poppins-regular'>
 
         <h1 className='text-[3.5vw]'>Sign In with your wallet</h1>
-        <h2 className='w-[30%] text-center text-[1.2vw] text-[#2d2d2ddf]'>Connect your metamask wallet for the smoothe something of the data</h2>
+        <h2 className='w-[30%] text-center text-[1.2vw] text-[#2d2d2ddf]'>Connect your metamask wallet to efficiate Better Funds</h2>
         <button onClick={click} className='flex bg-[#0066ff] text-white text-[1.5vw] px-4 py-2 rounded-xl gap-4 items-center justify-between m-4 hover:scale-[110%] transition-all duration-200'><Image src="/metamask.png" alt="" width={1080} height={1080} className='w-10 h-10'/> METAMASK </button>
     </div>
   )
