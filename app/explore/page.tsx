f24e69ac02dd45b4047576011e63ece3e6511aca
Ritin George <ritingeorge.d@gmail.com>
2024-02-24 23:59:50 +0530

    feat: market place

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,48 +1,56 @@
-'use client'
-import React, { useEffect,useState } from 'react'
-import NavBar from '../components/NavBar'
-import Explorecomp from '../components/Explorecomp'
-import { DocumentData, collection, onSnapshot } from 'firebase/firestore'
-import { db } from '@/firebase'
+"use client";
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import Explorecomp from "../components/Explorecomp";
+import { DocumentData, collection, onSnapshot } from "firebase/firestore";
+import { db } from "@/firebase";
 
 type docData = {
-  id: string
-  value: DocumentData
-}
+  id: string;
+  value: DocumentData;
+};
 
 function Page() {
-  const [data, setdata] = useState<docData[]>()
+  const [data, setdata] = useState<docData[]>();
 
   useEffect(() => {
-
-    
-
-    const unsubscribe = onSnapshot(collection( db, 'projects'),(snapshot) => {
-      setdata( snapshot.docs.map((doc) => {
-        
-        return { id: doc.id, value: doc.data()}
-       
-      }))
-      
-    })
-    return unsubscribe
-  },[])
-
+    const unsubscribe = onSnapshot(collection(db, "projects"), (snapshot) => {
+      setdata(
+        snapshot.docs.map((doc) => {
+          return { id: doc.id, value: doc.data() };
+        })
+      );
+    });
+    return unsubscribe;
+  }, []);
 
   return (
-    <div className='w-full h-full'>
-        <NavBar/>
-        <div className='flex flex-col items-center p-[2vw]'>
-            <h1 className='font-bold text-4xl'>Explore projects</h1>
-            <div className='flex justify-between flex-wrap gap-[2vw] my-10'>
-              {data?.map((doc) => {
-              {console.log(doc.value.coverImage)}
-                return <Explorecomp key={doc.id} imgUrl={doc.value.coverImage} projectName={doc.value.name} desc={doc.value.desc} contributed={doc.value.contributors} raised={doc.value.totalContributed} goal={doc.value['milestone 3 cost']} days={20}/>
-              })}
-            </div>
+    <div className="w-full h-full">
+      <NavBar />
+      <div className="flex flex-col items-center p-[2vw]">
+        <h1 className="font-bold text-4xl">Explore projects</h1>
+        <div className="flex justify-between flex-wrap gap-[2vw] my-10">
+          {data?.map((doc) => {
+            {
+              console.log(doc.value.coverImage);
+            }
+            return (
+              <Explorecomp
+                key={doc.id}
+                imgUrl={doc.value.coverImage}
+                projectName={doc.value.name}
+                desc={doc.value.desc}
+                contributed={doc.value.contributors}
+                raised={doc.value.totalContributed}
+                goal={doc.value["milestone 3 cost"]}
+                days={20}
+              />
+            );
+          })}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
